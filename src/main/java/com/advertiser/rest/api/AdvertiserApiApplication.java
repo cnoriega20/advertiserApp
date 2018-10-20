56312939014b40024e49245dedd4e85228ed355b
cnoriega20 <c79.noriega@gmail.com>
2018-10-20 01:59:13 -0500

    first commit: Springboot project structure per build.gradle

---
+++ src/main/java/com/advertiser/rest/api/AdvertiserApiApplication.java
@@ -0,0 +1,12 @@
+package com.advertiser.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertiserApiApplication.class, args);
+	}
+}
